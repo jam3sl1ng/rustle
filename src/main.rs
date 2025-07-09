05a132636d3d985691c27a539e0a0d94c7997731
jam3sl1ng <jamesling596@gmail.com>
2025-07-09 21:51:54 +0100

    V1.1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use colored::*;
+use bracket_random::prelude::RandomNumberGenerator;
+use std::collections::HashSet;
+
+const ALL_WORDS: &str = include_str!("assets/words.txt");
+
+fn clean_word(word: &str) -> String {
+    word.trim()
+        .to_uppercase()
+        .chars()
+        .filter(|c| c.is_ascii_alphabetic())
+        .collect()
+}
+
 fn main() {
     println!("Hello, world!");
 }
